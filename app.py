b0f82e13a0828bee2fd565fbebd5807f10e4b8c9
Ankit Mehta <rimabharti66@gmail.com>
2025-03-13 20:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,7 @@ import requests
 app = Flask(__name__)
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "X-IG-App-ID": "936619743392459",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive",
-    "Host": "www.instagram.com",
-    "Referer": "https://www.instagram.com/",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1"
+    "X-IG-App-ID": "936619743392459"
 }
 
 fetch_profile = lambda username: requests.get(
