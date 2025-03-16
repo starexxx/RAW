665b30e4ec7fe37b6826302bc5ebe9563d10b2f3
Ankit Mehta <183965198+starexxx@users.noreply.github.com>
2025-03-16 14:41:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,19 +15,23 @@ app.get("/", (req, res) => {
     fs.readdir(FILE_DIR, (err, files) => {
         if (err) return res.status(500).send("Error reading directory");
 
-        let fileList = files
-            .map((file, index) => ({
-                number: index + 1,
-                name: path.parse(file).name
-            }));
+        let fileList = files.map((file, index) => ({
+            number: index + 1,
+            name: file
+        }));
 
         res.render("index", { files: fileList });
     });
 });
 
 app.get("/app/:filename", (req, res) => {
-    let filePath = path.join(FILE_DIR, req.params.filename + ".html");
-    fs.existsSync(filePath) ? res.sendFile(filePath) : res.status(404).send("File Not Found");
+    let filePath = path.join(FILE_DIR, req.params.filename);
+
+    if (fs.existsSync(filePath)) {
+        res.sendFile(filePath);
+    } else {
+        res.status(404).send("<h1>404 - File Not Found</h1>");
+    }
 });
 
 app.listen(PORT, () => {
